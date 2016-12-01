ebc35dee3c9818db22661087f451de8de33b4504
Quentin Schwitzguébel <negus1309@gmail.com>
2016-12-01 14:45:03 +0100

    env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
         if(env('APP_ENV') !== 'local')
